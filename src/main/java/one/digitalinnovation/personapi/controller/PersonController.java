286cc006e982bf199e3dab7454fa9bd5b08d1d24
SamuelDino <samuel_qualitats@hotmail.com>
2021-11-06 14:34:07 -0300

    refactor findAll

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -5,6 +5,7 @@ import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,16 +35,8 @@ public class PersonController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<PersonDTO>> findByAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "firstName") String orderBy
-            ){
-
-            PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-
-            return ResponseEntity.ok(personService.findByAll(pageRequest));
+    public ResponseEntity<Page<PersonDTO>> findByAll(Pageable pageable ){
+        return ResponseEntity.ok(personService.findAll(pageable));
     }
 
     @DeleteMapping(value = "/{id}")
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -9,6 +9,7 @@ import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,8 +39,8 @@ public class PersonService {
     }
 
     @Transactional(readOnly = true)
-    public Page<PersonDTO> findByAll(PageRequest pageRequest) {
-        return personRepository.findAll(pageRequest).map(x->PersonMapper.INSTANCE.toDto(x));
+    public Page<PersonDTO> findAll(Pageable pageable) {
+        return personRepository.findAll(pageable).map(x->PersonMapper.INSTANCE.toDto(x));
     }
 
     @Transactional
